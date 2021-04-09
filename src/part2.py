811b73e5690685c8042407ddb918fe6e94379eb9
Erwin <e.meijerhof.1@student.rug.nl>
2021-04-09 21:57:46 +0200

    added text wrapping for tweets

--- src/part2.py
+++ src/part2.py
@@ -4,10 +4,10 @@ import json
 import tkinter as tk
 import tkinter.filedialog as fd
 import tkinter.ttk as ttk
+from tkinter.font import Font
 
-from nltk import download
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
+from nltk import download
 download('vader_lexicon')
 
 
@@ -58,13 +58,19 @@ class ConversationTreeview(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent)
 
+        self.font = Font(font='TkDefaultFont')
+        self.font_height = self.font.metrics('linespace')
+        self.style = ttk.Style(self)
+        self.style.configure('Treeview', 
+                             rowheight=((self.font_height * 2) + 10))
+
         self.scrollbar = ttk.Scrollbar(self)
         self.tree = ttk.Treeview(self, columns=('Tweet', 'Author',))
         self.scrollbar.configure(command=self.tree.yview)
         self.tree.configure(yscrollcommand=self.scrollbar.set)
 
-        self.tree.column('#1', width=100, stretch=0)
-        self.tree.column('#2', width=100, stretch=0)
+        self.tree.column('#1', width=120, stretch=0)
+        self.tree.column('#2', width=120, stretch=0)
 
         self.tree.heading('#0', text='Tweet')
         self.tree.heading('#1', text='Author')
@@ -73,27 +79,42 @@ class ConversationTreeview(tk.Frame):
         self.tree.pack(side='left', fill='both', expand=True)
         self.scrollbar.pack(side='right', fill='y')
 
-        self.selected_items = []
-        self.tree.bind("<<TreeviewSelect>>", self.on_select)
-
     def __clear(self):
         self.tree.delete(*self.tree.get_children())
 
-    def on_select(self, event):
-        for item in self.tree.selection():
-            print(self.tree.item(item, "text"))
+    def wrap_text(self, text):
+        text_length = len(text)
+        if not text_length > 50:
+            return text
+
+        line1 = []
+        line2 = []
+
+        for word in text.split():
+            if len(' '.join(line1) + ' ' + word) < (text_length/2):
+                line1.append(word)
+            else:
+                line2.append(word)
+
+        return ' '.join(line1) + '\n' + ' '.join(line2)
+
 
     def update(self, conversations):
         self.__clear()
 
         for convo in conversations:
-            root_tw = self.tree.insert('', 'end', text=convo.tweets[0],
+            root_tweet_text = self.wrap_text(convo.tweets[0])
+            root_tw = self.tree.insert('', 'end', text=root_tweet_text,
                                        values=[convo.authors[0],
                                                convo.conversation_sentiment])
             for i in range(1, convo.number_of_turns()):
-                self.tree.insert(root_tw, 'end', text=convo.tweets[i],
-                                 values=[convo.authors[i],
-                                         convo.sentiment_diffs[i-1]])
+                if convo.sentiment_diffs[i-1] <= 0:
+                    sent_diff = '+' + str(round(abs(convo.sentiment_diffs[i-1]), 5))
+                else:
+                    sent_diff = '-' + str(round(convo.sentiment_diffs[i-1], 5))
+                tweet_text = self.wrap_text(convo.tweets[i])
+                self.tree.insert(root_tw, 'end', text=tweet_text,
+                                 values=[convo.authors[i], sent_diff])
 
 
 class ConversationDisplay(tk.Frame):
@@ -113,6 +134,16 @@ class ConversationDisplay(tk.Frame):
                                        label="Max participants:",
                                        orient="horizontal")
         self.max_part_scale.set(10)
+
+        self.min_turn_scale = tk.Scale(self.filter_menu, from_=3, to=10,
+                                       label="Min length:",
+                                       orient="horizontal")
+        self.min_turn_scale.set(2)
+
+        self.max_turn_scale = tk.Scale(self.filter_menu, from_=3, to=10,
+                                       label="Max length:",
+                                       orient="horizontal")
+        self.max_turn_scale.set(10)
 
         self.sent_change_label = tk.Label(self.filter_menu, anchor="w",
                                           text="Change in sentiment:",
@@ -135,6 +166,8 @@ class ConversationDisplay(tk.Frame):
 
         self.min_part_scale.pack(fill='x')
         self.max_part_scale.pack(fill='x')
+        self.min_turn_scale.pack(fill='x')
+        self.max_turn_scale.pack(fill='x')
         self.sent_change_label.pack(fill='x')
         self.sent_change_opt.pack(fill='x')
         self.sent_thresh_scale.pack(fill='x')
@@ -146,11 +179,14 @@ class ConversationDisplay(tk.Frame):
     def __filter_conditions(self, convo):
         min_part = convo.unique_participants() >= self.min_part_scale.get()
         max_part = convo.unique_participants() <= self.max_part_scale.get()
+        min_turn = convo.number_of_turns() > self.min_turn_scale.get()
+        max_turn = convo.number_of_turns() > self.max_turn_scale.get()
         s_change = (self.sent_change_var.get() == "All" or
                     self.sent_change_var.get() == convo.conversation_sentiment)
         s_thr = convo.lowest_sentiment_diff() >= self.sent_thresh_scale.get()
 
-        return (min_part and max_part and s_change and s_thr)
+        return (min_part and max_part and min_turn and 
+                max_turn and  s_change and s_thr)
 
     def load_file(self):
         path = fd.askopenfilename(parent=self,
